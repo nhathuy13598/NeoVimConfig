e696c0656b57609f425690b3ea1ffa49c53d25e3
nhathuy <nhathuy13598@gmail.com>
2021-02-20 13:04:30 +0700

    Add zsh shell script and config file

---
+++ zsh.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install fonts-powerline
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
